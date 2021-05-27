63c6d6cbcb9a12f07c081d6288b8231d4086bffa
Jim Peters <jimjitsuesq@gmail.com>
2021-05-27 12:50:57 -0700

    Validation working
    Only still need to do conflicting activities

--- js/script.js
+++ js/script.js
@@ -12,9 +12,16 @@ function loadForm (element) {
     paypal.style.display = "none";
     bitcoin.style.display = "none";
     payment.value = "credit-card";
-    realTimeValidate(ccNum, ccNumRegex, ccNumHint);
-    realTimeValidate(zip, zipRegex, zipHint);
-    realTimeValidate(cvv, cvvRegex, cvvHint);
+    realTimeValidate(fullName, fullNameRegex, fullNameHint, fullNameField, fullNameHintText);
+    realTimeValidate(ccNum, ccNumRegex, ccNumHint, ccNumField, ccNumHintText);
+    realTimeValidate(zip, zipRegex, zipHint, zipField, zipHintText);
+    realTimeValidate(cvv, cvvRegex, cvvHint, cvvField, cvvHintText);
+    cbFocus(activitiesActive);
+    blurValidate(fullName, fullNameHint);
+    blurValidate(email, emailHint);
+    blurValidate(ccNum, ccNumHint);
+    blurValidate(zip, zipHint);
+    blurValidate(cvv, cvvHint);
 }
 
 // Variables to validate credit card payment info if selected as payment method
@@ -22,42 +29,70 @@ function loadForm (element) {
 const ccNum = document.getElementById('cc-num');
 const ccNumRegex = /^[0-9]{13,16}$/;
 const ccNumHint = document.getElementById('cc-hint');
+const ccNumField = "Credit Card Number";
+const ccNumHintText = ccNumHint.textContent;
 const zip = document.getElementById('zip');
 const zipRegex = /^[0-9]{5}$/
 const zipHint = document.getElementById('zip-hint');
+const zipField = "Zip Code";
+const zipHintText = zipHint.textContent;
 const cvv = document.getElementById('cvv');
 const cvvRegex = /^[0-9]{3}$/
 const cvvHint = document.getElementById('cvv-hint');
+const cvvField = "CVV Code";
+const cvvHintText = cvvHint.textContent;
 
-// Helper function for real-time validation
+// Helper function for real-time validation. Prevents triggering "not-valid" immediately upon focus on "keyup"
 
-function realTimeValidate (element, regex, hint) {
-    element.addEventListener('keyup', () => {
+function realTimeValidate (element, regex, hint, fieldName, hintText) {
+    element.addEventListener('keyup', (e) => {
+        if (e.keyCode === 9) {
+            e.preventDefault();
+        } else {
         if (regex.test(element.value) === false || element.value.length === 0) {
             element.parentElement.className = "not-valid";
-            //element.parentNode.appendChild(hint);
             hint.style.display = "block";
         } else {
             element.parentElement.className = "valid";
             hint.style.display = "none";
         }
+    }
+        if (element.value.length === 0) {
+            hint.innerHTML = `${fieldName} field cannot be blank.`;
+        } else {
+            hint.innerHTML = hintText;
+        }
+
     })
 }
 
-// Validate "name" field
+// Helper function for real-time validation if a user tabs away from a field without  completing it
 
-const nameHint = document.getElementById('name-hint');
-const regexName = /\w/
+function blurValidate (element, hint) {
+    element.addEventListener('blur', () => {
+        if (element.value.length === 0) {
+            element.parentElement.className = "not-valid";
+            hint.style.display = 'block';
+        }
+        })
+}
 
-realTimeValidate(fullName, regexName, nameHint);
+// Variables to validate "name" field
+
+const fullNameHint = document.getElementById('name-hint');
+const fullNameField = "Name";
+const fullNameHintText = fullNameHint.parentElement.textContent;
+const fullNameRegex = /\w/
 
 // Validate "e-mail" field
 
 const email = document.getElementById('email');
 const emailHint = document.getElementById('email-hint');
-const regexEmail = /^[^@]+@[^@.]+\.com$/i;
+const emailField = "Email";
+const emailHintText = emailHint.textContent;
+const emailRegex = /^[^@]+@[^@.]+\.com$/i;
 
-realTimeValidate(email, regexEmail, emailHint);
+realTimeValidate(email, emailRegex, emailHint, emailField, emailHintText);
 
 // Hide "other-job-role" input box by default and show only if user selects "other" from drop-down list
 
@@ -135,6 +170,19 @@ function CalculateTotalCost (arr, val, element1, element2) {
         })
     }
 }
+
+// Listen for focus and blur on checkboxes when tabbed to in "activities" section
+
+function cbFocus (arr) {
+    for (let i=0; i<arr.length; i++) {
+        arr[i].addEventListener('focus', e => {
+            e.target.parentElement.classList.add('focus');
+        })
+        arr[i].addEventListener('blur', e => {
+            e.target.parentElement.classList.remove('focus');
+        })
+    }
+}
    
 // Display alternate pay method if selected
 
@@ -167,18 +215,19 @@ function displayAlternatePayment (arr) {
 // Submit form and verify all required fields contain valid data
 
 const submitButton = document.getElementsByTagName('BUTTON');
+const form = document.getElementsByTagName("FORM");
 
 function submitValidate (element, hint) {
         if (element.parentElement.className !== "valid") {
             element.parentElement.className = "not-valid";
-            //element.parentNode.appendChild(hint);
             hint.style.display = "block";
         }
-    }
+}
 
 function submitForm (element) {
-    element[0].addEventListener('click', () => {
+    element[0].addEventListener('submit', (e) => {
         submitValidate(fullName, nameHint);
+        submitValidate(email, emailHint);
         if (payment.value === "credit-card") {
             submitValidate(ccNum, ccNumHint);
             submitValidate(zip, zipHint);
@@ -188,13 +237,15 @@ function submitForm (element) {
             activities.classList.add("not-valid");
             activitiesHint.style.display = "block";
         }
+        if ((fullName.parentElement.className === "not-valid") || (email.parentElement.className === "not-valid") || (ccNum.parentElement.className === "not-valid") || (zip.parentElement.className === "not-valid") || (cvv.parentElement.className === "not-valid") || (totalCost === 0)) {
+        e.preventDefault();
+        } 
     })
 }
-
 
 loadForm(body);
 hideotherJobRole(jobRoleOptions, otherJobRole);
 displayShirtColors(shirtDesignOptions, shirtColorOptions, ShirtColorDiv);
 CalculateTotalCost(activitiesActive, totalCost, activities, activitiesHint);
 displayAlternatePayment(paymentMethods);
-submitForm(submitButton);
+submitForm(form);
